bec38d9a0770389c5826ca86a7f6f628d79a4ec4
sollimann <kristoffer.solberg@cognite.com>
2021-02-07 13:50:30 +0100

    added a server for listening to traffic

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,33 +1,6 @@
-use std::collections::HashMap;
-use std::pin::Pin;
-use std::sync::Arc;
-use std::time::Instant;
-
-use futures::{Stream, StreamExt};
-use tokio::sync::mpsc;
-use tonic::transport::Server;
-use tonic::{Request, Response, Status};
+mod server;
 
 use drivers::roomba::drive;
-use roombasensors::roomba_sensors_server::{RoombaSensors, RoombaSensorsServer};
-use roombasensors::{LightBumper, Sensors, SensorsReceived, Stasis};
-
-#[derive(Debug)]
-struct RoombaSensorsService;
-
-pub mod roombasensors {
-    tonic::include_proto!("roombasensors");
-}
-
-#[tonic::async_trait]
-impl RoombaSensors for RoombaSensorsService {
-    async fn send_sensor_stream(
-        &self,
-        _request: Request<tonic::Streaming<Sensors>>,
-    ) -> Result<Response<SensorsReceived>, Status> {
-        unimplemented!()
-    }
-}
 
 fn main() {
     //reading::open_and_configure_port();
