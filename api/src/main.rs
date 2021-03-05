2ac3f3a0b349429920f40c9e49740ea2dc218717
sollimann <kristoffer.solberg@cognite.com>
2021-03-06 00:06:56 +0100

    make api crate a library, create setup as executable crate

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,3 @@
-mod client;
-mod server;
-
-use drivers::roomba::drive;
-
 fn main() {
-    //reading::open_and_configure_port();
-    //reading::list_ports();
-    //duplex::duplex();
-    drive::drive_and_sense();
-    //mode_commands();
+    println!("hello world!")
 }
---
+++ setup/src/main.rs
@@ -0,0 +1,9 @@
+use drivers::roomba::drive;
+
+fn main() {
+    //reading::open_and_configure_port();
+    //reading::list_ports();
+    //duplex::duplex();
+    drive::drive_and_sense();
+    //mode_commands();
+}
